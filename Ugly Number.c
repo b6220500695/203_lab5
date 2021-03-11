cc3a674897a4cee2a93602da1a35e8ec733fb4bf
mmint.m <pheeraya.p@ku.th>
2021-03-11 16:43:12 +0700

    ugly1

---
+++ Ugly Number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int min(int x, int y, int z){
+      if(x < z)
+         return x;
+      else
+         return z;
+   }
+   else{
+      if(y < z)
+         return y;
+      else
+         return z;
+   }
+
--- star.c
+++ star.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-   int n,nx,y=0,x=0,l=0,c=0,r=0;
-   scanf("%d",&n);
-   if(n%2==0) nx=n-1;
-   l = ceil(n/2.f)-1;
+   int N,nx,y=0,x=0,l=0,c=0,r=0;
+   scanf("%d",&N);
+   if(N%2==0)
+   nx=N-1;
+   l = ceil(N/2.f)-1;
    r = l;
-   for(y=0;y<=ceil(n/2.f)-1;y++,l--,r--,c++){
+   for(y=0;y<=ceil(N/2.f)-1;y++,l--,r--,c++){
          for(x=0;x<l;x++){
             printf("-");
          }
@@ -14,17 +15,19 @@ int main(){
          for(x=0; x<c; x++){
             printf("-");
          }
-         if(y!=0)printf("*");
+         if(y!=0)
+         printf("*");
         for(x=0;x<r;x++){
             printf("-");
         }
         printf("\n");
-        if(y>=1) c++;
+        if(y>=1)
+        c++;
    }
    c=c-2;
-    if(n%2==0){
+   if(N%2==0){
       x=0, l=0, r=0;
-        for(;y<n;y++,l++,r++,c--){
+        for(;y<N;y++,l++,r++,c--){
         for(x=0;x<l;x++){
             printf("-");
         }
@@ -32,30 +35,32 @@ int main(){
         for(x=0; x<c; x++){
             printf("-");
         }
-         if(y!=n-1)printf("*");
-        for(x=0;x<r;x++){
+         if(y!=N-1)
+         printf("*");
+         for(x=0;x<r;x++){
             printf("-");
         }
         printf("\n");
-        if(y!=n-2) c--;
+        if(y!=N-2)
+        c--;
         }
     }
-    else if(n%2!=0){
+    else if(N%2!=0){
         c=c-2, l=1, r=1;
-        for(;y<n;y++,l++,r++,c--){
+        for(;y<N;y++,l++,r++,c--){
         for(x=0;x<l;x++){
             printf("-");
         }
         printf("*");
-        for(x=0; x<c; x++){
-            printf("-");
+        for(x=0; x<c;x++){
+        printf("-");
         }
-        if(y!=n-1)printf("*");
+        if(y!=N-1)printf("*");
         for(x=0;x<r;x++){
             printf("-");
         }
         printf("\n");
-        if(y!=n-2) c--;
+        if(y!=N-2) c--;
         }
     }
 }
